8cf7a11ef4e0833f6f479da67cb238070531e001
geekduck <duck1218+github@gmail.com>
2019-01-08 22:52:17 +0900

    initial commit.

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import { Tooltip } from 'reactstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: false
+    };
+    this.forceRender = this.forceRender.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log(arguments);
+  }
+
+  forceRender() {
+    console.log("parent forceRender");
+    this.setState({isOn: !this.state.isOn});
+  }
+
+  render() {
+    console.log("parent render");
+    return (
+      <div>
+          <div>{`${this.state.isOn}`}</div>
+          <CustomTooltip isOn={this.state.isOn}>
+            <Counter count={10} parentForceRender={this.forceRender} />
+          </CustomTooltip>
+      </div>
+    );
+  }
+}
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    const count = props.count || 0;
+    this.state = {
+      count
+    };
+    this.countUp = this.countUp.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log(arguments);
+  }
+
+  componentDidMount() {
+    console.log("counter componentDidMount");
+  }
+
+  countUp() {
+    console.log("child countUp");
+    this.setState({
+      count: this.state.count + 1
+    });
+    this.props.parentForceRender();
+  }
+
+  render() {
+    console.log("child render");
+    return (
+      <div>
+        <div>{this.state.count}</div>
+        <Button color="info" onClick={this.countUp}>count up</Button>
+      </div>
+    );
+  }
+}
+
+class CustomTooltip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tooltipOpen: false
+    };
+    this.targetRef = React.createRef();
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
+  render() {
+    const {children, isOn} = this.props;
+    console.log(isOn);
+    return (
+      <React.Fragment>
+        <div ref={this.targetRef}>{children}</div>
+        <Tooltip placement="auto" isOpen={isOn} trigger={"hover"} isOpen={this.state.tooltipOpen} target={this.targetRef} toggle={this.toggle} delay={{show: 1000, hide:0}}>
+          {"Tooltip!!!"}
+        </Tooltip>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
